ca2771e7abaa3457e8d71e06297324635512919a
Sam Edwards <samdwar@amazon.com>
2023-07-01 10:28:28 +1000

    Update setup.sh to install docker-compose V2

--- setup.sh
+++ setup.sh
@@ -74,7 +74,8 @@ fi
 if [[ `command -v docker-compose` == "" ]]
 then
     echo "Installing docker-compose ..."
-    python3 -m pip install docker-compose
+    curl -L "https://github.com/docker/compose/releases/download/v2.7.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
 fi
 
 mkdir -p ~/.sagemaker
