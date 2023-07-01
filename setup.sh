342bcb6aa2c1e560048dd17dd712bb5c1554b294
Sam Edwards <samdwar@amazon.com>
2023-07-01 10:40:46 +1000

    Update setup.sh to get latest docker-compoase

--- setup.sh
+++ setup.sh
@@ -74,7 +74,7 @@ fi
 if [[ `command -v docker-compose` == "" ]]
 then
     echo "Installing docker-compose ..."
-    curl -L "https://github.com/docker/compose/releases/download/v2.7.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
 
